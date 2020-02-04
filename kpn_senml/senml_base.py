64dbb4125688de8d47c61c331c04447da0b0a2d2
Nikos Ftylitakis <nikos.ftylitakis@feron-tech.com>
2020-02-04 18:16:07 +0200

    Add dummy variable in senml_base to make the class usable when the file is minified.

--- kpn_senml/senml_base.py
+++ kpn_senml/senml_base.py
@@ -1,16 +1,17 @@
-# _  __  ____    _   _ 
+# _  __  ____    _   _
 # | |/ / |  _ \  | \ | |
 # | ' /  | |_) | |  \| |
 # | . \  |  __/  | |\  |
 # |_|\_\ |_|     |_| \_|
-# 
+#
 # (c) 2018 KPN
 # License: MIT license.
 # Author: Jan Bogaerts
-# 
+#
 # base for senml objects
 
 class SenmlBase(object):
     '''
     the base class for all senml objects.
     '''
+    dummyvar=1 # used to make the class usable if the file is minified
